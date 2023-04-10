0d7df6fa40387bc732e98978aee8da3fa921e35d
diya116 <dps1162004@gmail.com>
2023-04-11 02:08:33 +0530

    commit

--- app.js
+++ app.js
@@ -12,13 +12,35 @@ addcart.addEventListener("click",()=>{
 const slidebar=document.querySelector(".slidebar");
 const slider=document.querySelector(".main-slidebar");
 const close=document.querySelector(".close");
-slidebar.addEventListener("click",()=>{
-    console.log("clicked")
-    slider.style.display="block"
-    slider.style.transition="all 0.5s"
-})
+const searchbtn=document.querySelector(".search")
+const closebtn=document.querySelector(".close-search ")
+const searchmenu=document.querySelector(".slidebar-search")
+// slidebar.addEventListener("click",()=>{
+//     console.log("clicked")
+//     slider.style.display="block"
+//     slider.style.transition="all 0.5s"
+// })
 
-close.addEventListener("click",()=>{
-    console.log("lll");
-    slider.style.display="none"
-})
+// close.addEventListener("click",()=>{
+//     console.log("lll");
+//     slider.style.display="none"
+// })
+ searchbtn.addEventListener("click",()=>{
+     console.log("clicked")
+     searchmenu.style.display="block"
+     searchmenu.style.transition="all 0.5s"
+ })
+
+ closebtn.addEventListener("click",()=>{
+     console.log("lll");
+     searchmenu.style.display="none"
+ })
+function openNav()
+{
+    slider.style.width="250px"
+    console.log("clicked");
+}
+function closeNav()
+{
+    slider.style.width="0";
+}
